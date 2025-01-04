7e6a17870251f7c45d0e98c8f5c298f74be2b03b
Jaishree_k <128470432+Jai-shree@users.noreply.github.com>
2025-01-04 20:19:21 +0530

    list route done

--- src/list/list.module.ts
+++ src/list/list.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ListService } from './list.service';
 import { ListController } from './list.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PatientSchema } from 'src/schema/Patient.schema';
+import { Patient } from 'src/schema/Patient.schema';
 
 @Module({
+  imports:[
+    MongooseModule.forFeature([{
+      name: Patient.name,
+      schema: PatientSchema
+    }])
+  ],
   controllers: [ListController],
   providers: [ListService],
 })
--- src/list/list.service.ts
+++ src/list/list.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateListDto } from './dto/create-list.dto';
 import { UpdateListDto } from './dto/update-list.dto';
-import { IPatient } from 'src/interface/patient.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Patient } from 'src/schema/Patient.schema';
 import { Model } from 'mongoose';
 
 @Injectable()
 export class ListService {
+  constructor(@InjectModel(Patient.name) private patientModel:Model<Patient>){}
   create(createListDto: CreateListDto) {
     return 'This action adds a new list';
   }
 
   async findAll() {
-    const studentData =await this.patientModel.find();
-    if (!studentData || studentData.length == 0) {
-        throw 'Student data not found';
+    const data = await this.patientModel.find();
+    if (!data || data.length == 0) {
+        //throw new NotFoundException('Students data not found!');
+        console.log('Student data not found');
     }
-    return studentData;
+    return data;
   }
 
   findOne(id: number) {
