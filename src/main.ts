19fd1c8112b73c690e033b8f905f199a097681db
Jaishree_k <128470432+Jai-shree@users.noreply.github.com>
2025-01-05 12:53:53 +0530

    swagger

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpAdapterHost,Reflector } from '@nestjs/core';
 import { AllExceptionsFilter } from './allException.filter';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor} from '@nestjs/common';
+import { SwaggerTheme, SwaggerThemeNameEnum} from 'swagger-themes';
+import { DocumentBuilder , SwaggerModule} from '@nestjs/swagger';
 
 
 async function bootstrap() {
@@ -18,6 +20,19 @@ async function bootstrap() {
         excludeExtraneousValues: true,
       }),
     );
+    const swaggerTheme = new SwaggerTheme();
+    const swaggerConfig = new DocumentBuilder()
+    .setTitle('CS Hospitals')
+    .setDescription("Api documentation")
+    .setVersion('1.0.0')
+    .addTag('login', 'Login Routes')
+    .addTag('list', 'Paient Listing Routes')
+    .build();
+    
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('swagger', app, document, {
+      customCss: swaggerTheme.getBuffer(SwaggerThemeNameEnum.DARK),
+    });
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
