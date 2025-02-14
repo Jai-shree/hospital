d8dea5d82573f5d2876c4292e900f8a09a364257
Jai-shree <128470432+Jai-shree@users.noreply.github.com>
2025-02-14 15:54:59 +0530

    Update list.module.ts

--- src/list/list.module.ts
+++ src/list/list.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { ListService } from './list.service';
 import { ListController } from './list.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { PatientSchema } from 'src/schema/Patient.schema';
-import { Patient } from 'src/schema/Patient.schema';
+import { PatientSchema } from 'src/schema/patient.schema';
+import { Patient } from 'src/schema/patient.schema';
 
 @Module({
   imports:[
